890cfa4e31190035296d3d55beaeec32ff3c1303
gristen <67366234+gristen@users.noreply.github.com>
2023-04-01 19:20:43 +0300

    Update StorageServices.js

--- Services/StorageServices.js
+++ Services/StorageServices.js
@@ -3,20 +3,21 @@ const StorageModel = require('../models/StorageModel.js')
 
 class StatusServices
 {
+    //добавление нового склада
     async addStorage(req)
     {
        const storage = new StorageModel(req.body)
        await storage.save()
-        const message = "Склад успешно создан"
+        const message = "Склад успешно создан" //сообщение об успехе
         const result =
             {
                 message,
                 client
             }
-       return (result)
+       return (result) //отправляем объект
 
     }
-
+//получаем все склады
     async getAllStorage()
     {
          const storage= await StorageModel.find()
@@ -26,18 +27,21 @@ class StatusServices
                 message,
                 storage
             }
-        return (result)
+        return (result)//отправляем объект
     }
+    //удаляем 
     async deleteStorage(req)
     {
         await StorageModel.deleteOne(req.body)
         return ("Склад успешно удален.")
     }
+    //обновляем
     async updateStorage (req)
     {
          await StorageModel.findByIdAndUpdate(req.body._id,req.body,{new:true}) // ищем по ид и обновляем данные
         return ("Склад успешно обновлен.")
     }
+    //ищем по параметрам
    async findStorageByParam(query)
    {
        return (await StorageModel.find(query))
